44c0b9387c45cc497048986141ec59a8b5d7d42b
Taiga Ishii <taigaishii@TaiganoMacBook-Pro.local>
2018-07-29 15:47:58 +0900

    BmiService.php

--- app/Services/BmiService.php
+++ app/Services/BmiService.php
@@ -12,10 +12,34 @@ use App\Models\Person;
 
 class BmiService
 {
+  /*
+     * calc BMI from Person
+     *
+     * @param App\Models\Person $person
+     * @return float or void
+     */
     public static function getBmi(Person $person)
     {
+      return self::calcBmi($person->height, $person->weight);
     }
+
+    /*
+     * calc BMI
+     *
+     * @param float $height
+     * @param float $height
+     * @return float or void
+     */
+
     private static function calcBmi(float $height, float $weight)
     {
+        if( $height > 0 && $weight > 0 )
+        {
+            return $weight / $height / $height;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
