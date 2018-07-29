ebc4febcb920e11ec358225992b704f9d3580068
Taiga Ishii <taigaishii@TaiganoMacBook-Pro.local>
2018-07-29 15:39:32 +0900

    BmiServicesTest

---
+++ app/Services/BmiService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taigaishii
+ * Date: 2018/07/29
+ * Time: 15:23
+ */
+
+namespace App\Services;
+
+use App\Models\Person;
+
+class BmiService
+{
+    public static function getBmi(Person $person)
+    {
+    }
+    private static function calcBmi(float $height, float $weight)
+    {
+    }
+}
---
+++ tests/Unit/Services/BmiServiceTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Services\BmiService;
+use App\Models\Person;
+
+class BmiServiceTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * BMI caluc logic test
+     *
+     * @dataProvider bmiDataProvider
+     * @param $height
+     * @param $weight
+     * @param $result
+     * @return void
+     */
+    public function testCalcBmi($height, $weight, $result)
+    {
+        $bmiService    = new BmiService();
+        $method = new \ReflectionMethod(get_class($bmiService), 'calcBmi');
+        $method->setAccessible(true);
+        $actual = $method->invoke($bmiService, $height, $weight);
+        $this->assertEquals($result, $actual,'', 0.2);
+    }
+     /**
+     * BMI get logic test
+     *
+     * @dataProvider bmiDataProvider
+     * @param $height
+     * @param $weight
+     * @param $result
+     * @return void
+     */
+    public function testGetBmi($height, $weight, $result)
+    {
+        $person = new Person();
+        $person->height = $height;
+        $person->weight = $weight;
+        $actual = BmiService::getBmi($person);
+        $this->assertEquals($result, $actual,'', 0.2);
+    }
+     public function bmiDataProvider()
+    {
+        return [
+            [1.6,60,23.4735],
+            [1.8,80,24.6914],
+            [1,0,false],
+            [0,50,false],
+        ];
+    }
+}
