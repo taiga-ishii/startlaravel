e86fd1374296c1d2f4b57209f739c70c36bb3028
Taiga Ishii <taigaishii@TaiganoMacBook-Pro.local>
2018-07-29 15:54:16 +0900

    Mockey

--- tests/Feature/Route/PersonTest.php
+++ tests/Feature/Route/PersonTest.php
@@ -8,6 +8,17 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PersonTest extends TestCase
 {
+    protected $personMock;
+    public function setUp()
+    {
+      parent::setUp();
+      $this->personMock =  Mockery::mock('App\Models\Person');
+    }
+    public function tearDown()
+    {
+      parent::tearDown();
+      Mockery::close();
+    }
     /**
      * A basic test example.
      *
@@ -20,27 +31,59 @@ class PersonTest extends TestCase
 
     public function testPersonList()
    {
+        $this->personMock
+            ->shouldReceive('all')
+            ->once()
+            ->andReturn($this->personMock);
+        $this->app->instance('App\Models\Person', $this->personMock);
+
        $response = $this->json('GET','person');
        $response->assertStatus(200);
    }
     public function testPersonStore()
    {
+     $this->personMock
+           ->shouldReceive('save')
+           ->once()
+           ->andReturn(1);
+       $this->app->instance('App\Models\Person', $this->personMock);
        $response = $this->json('POST','person',
            ['name' => 'Johnny', 'weight' => '60', 'height' => '1.72']);
        $response->assertStatus(200);
    }
     public function testPersonView()
    {
+     $this->personMock
+         ->shouldReceive('resolveRouteBinding')
+         ->once()
+         ->andReturn($this->personMock);
+     $this->app->instance('App\Models\Person', $this->personMock);
        $response = $this->json('GET','person/1');
        $response->assertStatus(200);
    }
     public function testPersonUpdate()
    {
+     $this->personMock
+        ->shouldReceive('resolveRouteBinding')
+        ->once()
+        ->andReturn($this->personMock)
+        ->shouldReceive('save')
+        ->once()
+        ->andReturn($this->personMock);
+    $this->app->instance('App\Models\Person', $this->personMock);
        $response = $this->json('PUT','person/1', ['name' => 'Johnson']);
        $response->assertStatus(200);
    }
     public function testPersonDelete()
    {
+     $this->personMock
+          ->shouldReceive('resolveRouteBinding')
+          ->once()
+          ->andReturn($this->personMock)
+          ->shouldReceive('delete')
+          ->once()
+          ->andReturn(1);
+      $this->app->instance('App\Models\Person', $this->personMock);
        $response = $this->json('DELETE','person/1');
        $response->assertStatus(200);
    }
